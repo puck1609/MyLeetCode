8a1225919ab14b33c5d449dcea24b6ae1a535432
puck1609 <1609578991@qq.com>
2021-08-15 18:15:42 +0800

    Create XmlParser.java

---
+++ data_structure/XmlParser.java
@@ -0,0 +1,46 @@
+// XML解析器
+public class XmlParser {
+    private int i;
+    public String ParseXml(String xml, String path) {
+        String[] node = path.split("\\.");
+        for (String s : node) {
+            xml = parseXml(xml, s);
+        }
+        return xml;
+    }
+    private String parseXml(String xml, String e) {
+        i = 0;
+        int len = xml.length();
+        while (i < len) {
+            if (xml.charAt(i) == '<') {
+                String str = parseKey(xml);
+                if (str.equals(e)) break;
+            }
+            i++;
+        }
+        i++;
+        int l = i, r = -1;
+        while (i < len) {
+            if (i + 1 < len && xml.charAt(i) == '<' && xml.charAt(i + 1) == '/') {
+                i++;
+                String str = parseKey(xml);
+                if (str.equals(e)) {
+                    r = i - str.length() - 2;
+                    break;
+                }
+            }
+            i++;
+        }
+        if (r < 0) return "";
+        return xml.substring(l, r);
+    }
+    private String parseKey(String xml) {
+        int len = xml.length();
+        i++;
+        int j = i;
+        while (i < len && xml.charAt(i) != '>') {
+            i++;
+        }
+        return xml.substring(j, i);
+    }
+}
