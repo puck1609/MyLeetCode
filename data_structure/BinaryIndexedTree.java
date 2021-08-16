79155da06e36526031e5f7589ffbc224ca806679
puck1609 <1609578991@qq.com>
2021-08-16 16:31:29 +0800

    Update BinaryIndexedTree.java
    增加Set功能

--- data_structure/BinaryIndexedTree.java
+++ data_structure/BinaryIndexedTree.java
@@ -1,19 +1,25 @@
 public class BinaryIndexedTree {
-    int[] arr;
-    int len;
+    private int[] origin;
+    private int[] arr;
+    private int len;
     BinaryIndexedTree(int[] in) {
         len = in.length;
         arr = new int[len + 1];
+        origin = new int[len];
+        System.arraycopy(in, 0, origin, 0, len);
+        for (int i = 0; i < len; i++) {
+            Update(i + 1, origin[i]);
+        }
     }
-    void update(int x, int add) {
-        if (x > len) return;
+    public void Update(int x, int add) {
+        if (x > len || x < 1) return;
         while (x <= len) {
             arr[x] += add;
             x += lowbit(x);
         }
     }
-    int query(int x) {
-        if (x > len) return -1;
+    public int Query(int x) {
+        if (x > len || x < 1) return 0;
         int res = 0;
         while (x > 0) {
             res += arr[x];
@@ -21,7 +27,13 @@ public class BinaryIndexedTree {
         }
         return res;
     }
-    int lowbit(int x) {
+    public void Set(int x, int val) {
+        if (x > len || x < 1) return;
+        int add = val - origin[x - 1];
+        origin[x - 1] = val;
+        Update(x, add);
+    }
+    private int lowbit(int x) {
         return x & (-x);
     }
 }
